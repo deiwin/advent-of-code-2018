c2c452902942b42cb0fb49b4b3a4c29f587bf43a
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2018-12-07 14:35:25 +0200

    Solve part 2 of day 7
    This got pretty messy.

--- day7.hs
+++ day7.hs
@@ -3,7 +3,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Data.Char (ord, chr)
-import Data.List (reverse, notElem)
+import Data.List (reverse, notElem, partition, sort, repeat)
 import Data.Graph (Graph, Edge, Vertex, buildG, indegree, edges)
 import Data.Array.IArray (elems, assocs, bounds, array)
 
@@ -12,6 +12,7 @@ main = do
     input <- lines <$> readFile "day7.input"
     let graph = buildGraph $ parseLine <$> input
     print $ getOrder graph
+    print $ parallelAlphaTopSort graph
 
 getOrder :: Graph -> String
 getOrder g = chr <$> alphaTopSort g
@@ -30,6 +31,29 @@ alphaTopSort' g acc
     removable x = noIncoming x && fst x `notElem` acc
     noIncoming (_, 0) = True
     noIncoming _      = False
+
+parallelAlphaTopSort :: Graph -> Int
+parallelAlphaTopSort g = parallelAlphaTopSort' g 0 [] []
+parallelAlphaTopSort' :: Graph -> Int -> [(Vertex, Int)] -> [Vertex] -> Int
+parallelAlphaTopSort' g elapsedTime wip acc
+  | length (elems g) == length acc && null wip = elapsedTime
+  | null wip = parallelAlphaTopSort' newG elapsedTime nextWip newAcc
+  | otherwise = parallelAlphaTopSort' newG nextTime nextWip newAcc
+  where
+    newG = array (bounds g) newAssocs
+    newAssocs = zip (fst <$> finishedWork) (repeat []) ++ filter ((`notElem` (fst <$> finishedWork)) . fst) (assocs g)
+    (finishedWork, inProgressWork) = partition ((<= 0) . snd) (doWork <$> wip)
+    doWork (v, x) = (v, x - 1)
+    nextTime = elapsedTime + 1
+    nextWip = inProgressWork ++ zip newWork (workTime <$> newWork)
+    newAcc = newWork ++ acc
+    newWork = take freeWorkerCount allRemovable
+    freeWorkerCount = 5 - length inProgressWork
+    allRemovable = sort $ fmap fst $ filter removable $ assocs $ indegree newG
+    removable x = noIncoming x && fst x `notElem` acc
+    noIncoming (_, 0) = True
+    noIncoming _      = False
+    workTime v = v - 4
 
 buildGraph :: [Edge] -> Graph
 buildGraph = buildG (ord 'A', ord 'Z')
