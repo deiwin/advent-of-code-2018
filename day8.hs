85b3ff054fa242d8510e72779f9c16d6dc09547c
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2018-12-08 08:53:36 +0200

    Solve part 2 of day 8

--- day8.hs
+++ day8.hs
@@ -3,7 +3,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Data.Graph (Forest, Tree(..))
-import Data.List (foldl')
+import Data.List (foldl', (!!))
 import Data.Foldable (foldMap)
 import Data.Monoid (Sum(..))
 
@@ -12,13 +12,21 @@ main = do
     input <- words . head . lines <$> readFile "day8.input"
     let (root, _) = readTree $ read <$> input
     print $ getSum $ foldMap (Sum . sum) root
+    print $ treeVal root
+
+treeVal :: Tree [Int] -> Int
+treeVal (Node metadata []) = sum metadata
+treeVal (Node metadata ts) = sum childrenVals
+  where
+    childrenVals = treeVal . (ts !!) <$> safeIndices
+    safeIndices  = filter isSafeIndex $ (+ (-1)) <$> metadata
+    isSafeIndex i = i >= 0 && i < length ts
 
 readTree :: [Int] -> (Tree [Int], [Int])
 readTree (children : metaCount : rest) = (Node metadata ts, remainder)
     where (ts, metadata, remainder) = readChildren children metaCount rest []
 
 readChildren :: Int -> Int -> [Int] -> [Tree [Int]] -> ([Tree [Int]], [Int], [Int])
-readChildren 0        metaCount input ts = (ts, take metaCount input, drop metaCount input)
+readChildren 0        metaCount input ts = (reverse ts, take metaCount input, drop metaCount input)
 readChildren children metaCount input ts = readChildren (children - 1) metaCount remainder (t : ts)
     where (t, remainder) = readTree input
-
