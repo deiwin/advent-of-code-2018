077b8791d2a53736d36705b11cb269ae726af7e8
Deiwin Sarjas <deiwin.sarjas@gmail.com>
2018-12-26 20:48:01 +0200

    Solve part 2 of day 16

--- day16.hs
+++ day16.hs
@@ -4,14 +4,16 @@
 {-# LANGUAGE TupleSections #-}
 
 import qualified Data.Vector.Unboxed as VU
+import qualified Data.Map.Strict as M
+import qualified Data.Set as S
 import Data.List.Split (chunksOf)
-import Data.List (isPrefixOf)
+import Data.List (isPrefixOf, foldl', partition)
 import Data.Bits ((.&.), (.|.))
 
 type Registers = VU.Vector Int
 type Instruction = (Op, InstructionInput)
 type InstructionInput = (Int, Int, Int)
-type Sample = (Registers, InstructionInput, Registers)
+type Sample = (Registers, Int, InstructionInput, Registers)
 
 data Op = Addr | Addi
         | Mulr | Muli
@@ -24,14 +26,45 @@ data Op = Addr | Addi
 
 main :: IO ()
 main = do
-    samples <- parseInput <$> readFile "day16.input"
+    (samples, code) <- parseInput <$> readFile "day16.input"
     print $ countLikeThreeOpcodes samples
+    print $ executionResult samples code
+
+executionResult :: [Sample] -> [[Int]] -> Int
+executionResult samples code = finalRegisters VU.! 0
+  where
+    finalRegisters = foldl' exec initialRegisters instructions
+    initialRegisters = VU.fromList [0, 0, 0, 0]
+    opCodeMap = findOpcodes samples
+    instructions = toInstruction <$> code
+    toInstruction [opCode, a, b, c] = (opCodeMap M.! opCode, (a, b, c))
+
+findOpcodes :: [Sample] -> M.Map Int Op
+findOpcodes samples = M.map singular finalOpSets
+  where
+    singular ops = if S.size ops == 1
+                      then head $ S.toList ops
+                      else error "More than one for op"
+    finalOpSets = head $ dropWhile notFinal $ iterate tick initialOpSets
+    tick m = flip M.union m $ reduce $ partition ((== 1) . S.size . snd) $ M.assocs m
+    reduce (finalOps, others) = let finishedOps = S.unions $ snd <$> finalOps
+                                 in M.fromList $ remove finishedOps <$> others
+    remove finishedOps (opCode, ops) = (opCode, ops S.\\ finishedOps)
+    notFinal m = any (> 1) $ S.size <$> M.elems m
+    initialOpSets = foldl' f M.empty samples
+    f :: M.Map Int (S.Set Op) -> Sample -> M.Map Int (S.Set Op)
+    f m s@(_, opCode, _, _) = M.insert opCode (newEntry m s) m
+    newEntry m s@(_, opCode, _, _) = let ops = S.fromList (likeOps s)
+                                      in maybe ops (S.intersection ops) (m M.!? opCode)
 
 countLikeThreeOpcodes :: [Sample] -> Int
 countLikeThreeOpcodes samples = length $ filter likeThreeOrMore samples
   where
     likeThreeOrMore s = likeCount s >= 3
-    likeCount (before, input, after) = length $ filter ((== after) . exec before . (, input)) $ enumFrom Addr
+    likeCount = length . likeOps
+
+likeOps :: Sample -> [Op]
+likeOps (before, _, input, after) = filter ((== after) . exec before . (, input)) $ enumFrom Addr
 
 exec :: Registers -> Instruction -> Registers
 exec rs i@(_, (_, _, c)) = rs VU.// [(c, go i)]
@@ -53,12 +86,14 @@ exec rs i@(_, (_, _, c)) = rs VU.// [(c, go i)]
     go (Eqri, (a, b, c)) = if (rs VU.! a) == b then 1 else 0
     go (Eqrr, (a, b, c)) = if (rs VU.! a) == (rs VU.! b) then 1 else 0
 
-parseInput :: String -> [Sample]
-parseInput input = fmap parseSample $ takeWhile isSample $ fmap (take 3) $ chunksOf 4 $ lines input
+parseInput :: String -> ([Sample], [[Int]])
+parseInput input = (parseSample <$> rawSamples, code)
   where
+    (rawSamples, rawCode) = span isSample $ chunksOf 4 $ lines input
+    code = fmap read . words <$> dropWhile null (concat rawCode)
     isSample (head:_) = "Before: " `isPrefixOf` head
-    parseSample [before, input, after] = let parsedBefore = read $ dropWhile (/= '[') before
-                                             [opCode, a, b, c] = read <$> words input
-                                             instructionInput = (a, b, c)
-                                             parsedAfter = read $ dropWhile (/= '[') after
-                                          in (parsedBefore, instructionInput, parsedAfter)
+    parseSample [before, input, after, _] = let parsedBefore = read $ dropWhile (/= '[') before
+                                                [opCode, a, b, c] = read <$> words input
+                                                instructionInput = (a, b, c)
+                                                parsedAfter = read $ dropWhile (/= '[') after
+                                             in (parsedBefore, opCode, instructionInput, parsedAfter)
